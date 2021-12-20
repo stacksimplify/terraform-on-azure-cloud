e8df1c7d9d425e8984a012b9e211bc08e230d0c3
Pankaj <pankajazlearn@gmail.com>
2021-12-20 11:48:46 +0000

    Test added aws provider

--- 05-Terraform-Provider-Resource-Block-Basics/terraform-manifests/c1-versions.tf
+++ 05-Terraform-Provider-Resource-Block-Basics/terraform-manifests/c1-versions.tf
@@ -10,6 +10,10 @@ terraform {
       #version = ">= 2.0.0, <= 2.64.0"   
       #version = "~> 2.64" # For Production grade              
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = ">=2.0"
+    }
   }
 }
 # Provider Block
