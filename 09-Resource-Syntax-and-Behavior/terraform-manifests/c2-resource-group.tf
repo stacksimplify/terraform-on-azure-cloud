44e5a91c925dcb966dea8529a4adf8c79da58767
ajgcecloud1 <ajgcecloud1@gmail.com>
2025-02-06 15:23:22 -0500

    TF resources update

--- 09-Resource-Syntax-and-Behavior/terraform-manifests/c2-resource-group.tf
+++ 09-Resource-Syntax-and-Behavior/terraform-manifests/c2-resource-group.tf
@@ -1,5 +1,5 @@
 # Resource-1: Azure Resource Group
-resource "azurerm_resource_group" "myrg" {
-  name = "myrg-1"
+resource "azurerm_resource_group" "dev-terraform-rg1" {
+  name     = "dev-terraform-rg1"
   location = "East US"
 }
--- 09-Resource-Syntax-and-Behavior/terraform-manifests/c3-virtual-network.tf
+++ 09-Resource-Syntax-and-Behavior/terraform-manifests/c3-virtual-network.tf
@@ -1,28 +1,28 @@
 # Resource-2: Create Virtual Network
-resource "azurerm_virtual_network" "myvnet" {
-  name                = "myvnet-1"
+resource "azurerm_virtual_network" "dev-terraform-vnet" {
+  name                = "dev-terraform-vnet-1"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.myrg.location
-  resource_group_name = azurerm_resource_group.myrg.name
+  location            = azurerm_resource_group.dev-terraform-rg1.location
+  resource_group_name = azurerm_resource_group.dev-terraform-rg1.name
   tags = {
-    "Name" = "myvnet-1"
+    "Name" = "dev-terraform-vnet-1"
     #"Environment" = "Dev"  # Uncomment during Step-10
   }
 }
 
 # Resource-3: Create Subnet
-resource "azurerm_subnet" "mysubnet" {
-  name                 = "mysubnet-1"
-  resource_group_name  = azurerm_resource_group.myrg.name
-  virtual_network_name = azurerm_virtual_network.myvnet.name
+resource "azurerm_subnet" "dev-terraform-subnet-1" {
+  name                 = "dev-terraform-subnet"
+  resource_group_name  = azurerm_resource_group.dev-terraform-rg1.name
+  virtual_network_name = azurerm_virtual_network.dev-terraform-vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 # Resource-4: Create Public IP Address
-resource "azurerm_public_ip" "mypublicip" {
-  name                = "mypublicip-1"
-  resource_group_name = azurerm_resource_group.myrg.name
-  location            = azurerm_resource_group.myrg.location
+resource "azurerm_public_ip" "dev-terraform-publicip" {
+  name                = "dev-terraform-publicip-1"
+  resource_group_name = azurerm_resource_group.dev-terraform-rg1.name
+  location            = azurerm_resource_group.dev-terraform-rg1.location
   allocation_method   = "Static"
   tags = {
     environment = "Dev"
@@ -30,16 +30,16 @@ resource "azurerm_public_ip" "mypublicip" {
 }
 
 # Resource-5: Create Network Interface
-resource "azurerm_network_interface" "myvmnic" {
-  name                = "vmnic"
-  location            = azurerm_resource_group.myrg.location
-  resource_group_name = azurerm_resource_group.myrg.name
+resource "azurerm_network_interface" "dev-terraform-vmnic-1" {
+  name                = "dev-terraform-vmnic"
+  location            = azurerm_resource_group.dev-terraform-rg1.location
+  resource_group_name = azurerm_resource_group.dev-terraform-rg1.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.mysubnet.id
+    subnet_id                     = azurerm_subnet.dev-terraform-subnet-1.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.mypublicip.id 
+    public_ip_address_id          = azurerm_public_ip.dev-terraform-publicip.id
   }
 }
 
