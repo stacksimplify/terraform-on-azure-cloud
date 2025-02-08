0436e8fb118ace850b822971c21ba8425f730170
ajgcecloud1 <ajgcecloud1@gmail.com>
2025-02-07 22:26:23 -0500

    section-10 config push on azure

--- 10-Azure-Virtual-Network-4Tier/terraform-manifests/c1-versions.tf
+++ 10-Azure-Virtual-Network-4Tier/terraform-manifests/c1-versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.0" 
+      source  = "hashicorp/azurerm"
+      version = ">= 2.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = ">= 3.0"
     }
   }
@@ -15,7 +15,8 @@ terraform {
 
 # Provider Block
 provider "azurerm" {
- features {}          
+  features {}
+  subscription_id = "cd10533a-c677-4d76-bda9-b7234d3c33de"
 }
 
 
--- 10-Azure-Virtual-Network-4Tier/terraform-manifests/c6-02-virtual-network.tf
+++ 10-Azure-Virtual-Network-4Tier/terraform-manifests/c6-02-virtual-network.tf
@@ -4,5 +4,5 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = var.vnet_address_space
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  tags = local.common_tags
+  tags                = local.common_tags
 }
